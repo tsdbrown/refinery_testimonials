72396428158bc7fd7db2c2a2bbb9f764b2ccc970
Luke Brown <luke.brown@transcendit.co.uk>
2010-07-15 18:55:44 +0100

    Added required path prefix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :testimonials, :only => [:index]
 
-  map.namespace(:admin) do |admin|
+  map.namespace(:admin, :path_prefix => 'refinery') do |admin|
     admin.resources :testimonials
   end
 end
